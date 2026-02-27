bcd542b1420f44b8555c628183a7e6aec011ad94
Frank Fru <chifru19@googlemail.com>
2026-02-27 21:56:57 +0100

    SEC: Hardened S3 bucket to resolve Checkov security failures

--- main.tf
+++ main.tf
@@ -13,4 +13,34 @@ provider "aws" {
 
 resource "aws_s3_bucket" "security_logs" {
   bucket = "cloud-security-logs-bucket"
+}resource "aws_s3_bucket" "security_logs" {
+  bucket = "cloud-security-logs-bucket"
+}
+
+# Fixes CKV_AWS_21 (Versioning)
+resource "aws_s3_bucket_versioning" "logs_versioning" {
+  bucket = aws_s3_bucket.security_logs.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Fixes CKV_AWS_145 (Encryption)
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs_encryption" {
+  bucket = aws_s3_bucket.security_logs.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
+# Fixes CKV2_AWS_6 (Public Access Block)
+resource "aws_s3_bucket_public_access_block" "logs_access" {
+  bucket = aws_s3_bucket.security_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
